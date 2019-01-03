53b6a85348dffa649ba468280bd42d50efe8f1de
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-01-03 18:56:45 +0530

    uploaded

--- classExample/main.swift
+++ classExample/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+class Poly {
+    
+    public var width:Int = 0
+    var height:Int = 0
+  
+    func SetPoints(a:Int,b:Int)  {
+        width = a
+        height = b
+    }
+    
+}
 
+//var p = Poly()
+class Rect: Poly {
+    func FindArea()  {
+        print(width * height)
+    }
+}
+
+class Tri: Poly {
+    func FindArea()  {
+        print(width * height / 2)
+    }
+}
+
+
+var rectObj = Rect()
+var triObj = Tri()
+
+rectObj.SetPoints(a: 4, b: 4)
+triObj.SetPoints(a: 5, b: 6)
+
+
+rectObj.FindArea()
+
+triObj.FindArea()
